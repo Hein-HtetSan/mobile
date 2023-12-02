37bce5f3cb44bdcc7fbec7ebc13fcdec4c284e59
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-12-02 21:50:01 +0630

    update by zed

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import Screens from './screens';
+// import the Apollo libraries
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
+// import SecureStore for retrieving the token value
+import * as SecureStore from 'expo-secure-store';
+
+// import environment configuration
+import getEnvVars from '../config';
+const { API_URI } = getEnvVars();
+
+// configure our API URI & cache
+const uri = API_URI;
+const cache = new InMemoryCache();
+const httpLink = createHttpLink({ uri });
+
+// return the headers to the context
+const authLink = setContext(async (_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: (await SecureStore.getItemAsync('token')) || ''
+    }
+  };
+});
+
+// configure Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache
+});
 
 const Main = () => {
+  // wrap our app in the ApolloProvider higher-order component
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello world!</Text>
-    </View>
+    <ApolloProvider client={client}>
+      <Screens />
+    </ApolloProvider>
   );
 };
 
